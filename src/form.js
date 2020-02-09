4e5abc90414e71c9894cbad18fb0f7b1fc033d60
Student <test@test.pl>
2020-02-09 15:32:23 +0100

    form commit: 1

--- src/form.js
+++ src/form.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+    validateForm = (event) => {
+        var x = document.forms["myForm"]["inputName"].value;
+        if (x == "") {
+            alert("Name must be filled out");
+            return false;
+        }
+    }
+
     render() {
         return (
-            <div>
-                <h2>Form</h2>
-            </div>);
+            <form name="myForm" onSubmit={this.validateForm}>
+                <p>Enter your name:</p>
+                <input
+                    type="text"
+                    name="inputName"
+                />
+                <input type="submit" value="Submit!"/>
+
+            </form>
+
+        )
     }
+
 }
+
+
+
+
+
