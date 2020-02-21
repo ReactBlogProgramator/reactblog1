a81ed83bfe18bb9d4b0888993b3eef4a15e1a962
pmmagusiak <pmmagusiak@gmail.com>
2020-02-21 20:50:06 +0100

    form commit

--- src/form.js
+++ src/form.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import "./form.css"
 
 export default class Form extends React.Component {
     validateForm = (event) => {
-        var x = document.forms["myForm"]["inputName"].value;
-        if (x == "") {
-            alert("Name must be filled out");
+        var x = document.forms["myForm"]["articleID"].value;
+        var y = document.forms["myForm"]["articleTitle"].value;
+        var z = document.forms["myForm"]["articleTxt"].value;
+        if (x === "" || y === "" || z === "") {
+            alert("All data must be filled out!");
             return false;
         }
     }
 
     render() {
         return (
-            <form name="myForm" onSubmit={this.validateForm}>
-                <p>Enter your name:</p>
-                <input
-                    type="text"
-                    name="inputName"
-                />
-                <input type="submit" value="Submit!"/>
+            <div className="form-container">
+                <div className="form">
+                    <h1>Article addition form</h1>
+                    <form className="myForm" name="myForm" onSubmit={this.validateForm}>
+                        <p>Enter article ID:</p>
+                        <input type="text" name="articleID"/>
+                        <p>Enter article title:</p>
+                        <input type="text" name="articleTitle"/>
+                        <p>Enter article text:</p>
+                        <textarea name="articleTxt" style={{height:400}}/><br/><br/>
+                        <input type="submit" value="Submit article"/>
+                    </form>
+                </div>
+            </div>
 
-            </form>
-
-        )
+        );
     }
 
 }
-
-
-
-
-
